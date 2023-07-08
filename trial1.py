3777a45314d38e5b0bedeb5b008bcf827f24d68a
Alexei <alebook111@gmail.com>
2023-07-09 00:54:13 +0400

    start incapsulation

--- trial1.py
+++ trial1.py
@@ -8,7 +8,7 @@ class Server:
         self.link = None
 
     def send_data(self, data):
-        self.link.buffer[data.ip] = self.link.buffer.get(data.ip, []) + [data.data]
+        self.link.buffer += [data]
 
 
     def get_data(self):
@@ -25,20 +25,22 @@ class Router:
     def __init__(self, c=None):
         if c is None:
             self.all_connections = {}
-            self.buffer = {}
+            self.buffer = []
 
     def link(self, server):
         server.link = self  # Делаем ссылку на объект класса Роутер для каждого сервера после его подключения
         self.all_connections[server.IP] = server
 
     def unlink(self, server):
-        del self.all_connections[server.get_ip]
+        del self.all_connections[server.get_ip()]
 
     def send_data(self):
-        while len(self.buffer):
-            i = list(self.buffer.keys())[0]
-            self.all_connections[i].buffer.extend(self.buffer[i])
-            del self.buffer[i]
+        while self.buffer:
+            i = self.buffer.pop()
+            if i.ip in self.all_connections:
+                self.all_connections[i.ip].buffer.append(i)
+
+
 
 
 class Data:
@@ -74,11 +76,11 @@ class Data:
 # print(s4.get_data())
 # print(router.buffer)
 
-
-assert hasattr(Router, 'link') and hasattr(Router, 'unlink') and hasattr(Router,
-                                                                         'send_data'), "в классе Router присутсвутю не все методы, указанные в задании"
-assert hasattr(Server, 'send_data') and hasattr(Server, 'get_data') and hasattr(Server,
-                                                                                'get_ip'), "в классе Server присутсвутю не все методы, указанные в задании"
+#
+# assert hasattr(Router, 'link') and hasattr(Router, 'unlink') and hasattr(Router,
+#                                                                          'send_data'), "в классе Router присутсвутю не все методы, указанные в задании"
+# assert hasattr(Server, 'send_data') and hasattr(Server, 'get_data') and hasattr(Server,
+#                                                                                 'get_ip'), "в классе Server присутсвутю не все методы, указанные в задании"
 
 router = Router()
 sv_from = Server()
@@ -92,12 +94,18 @@ router.link(sv_to)
 sv_from.send_data(Data("Hello", sv_to.get_ip()))
 sv_from2.send_data(Data("Hello", sv_to.get_ip()))
 sv_to.send_data(Data("Hi", sv_from.get_ip()))
+print('buffer of router', router.buffer)
+
 router.send_data()
+print('данные после отправки из буфера роутера в сервере', sv_to.buffer)
+
+print(len(router.buffer))
 msg_lst_from = sv_from.get_data()
 msg_lst_to = sv_to.get_data()
-print(msg_lst_from[0])
+print(msg_lst_from[0].data)
 print(len(msg_lst_to))
-print(msg_lst_to[0])
+print(msg_lst_to[0].data, msg_lst_to[1].data)
+
 assert len(router.buffer) == 0, "после отправки сообщений буфер в роутере должен очищаться"
 assert len(sv_from.buffer) == 0, "после получения сообщений буфер сервера должен очищаться"
 
@@ -108,9 +116,17 @@ assert msg_lst_from[0].data == "Hi" and msg_lst_to[
 
 assert hasattr(router, 'buffer') and hasattr(sv_to,
                                              'buffer'), "в объектах классов Router и/или Server отсутствует локальный атрибут buffer"
-
+print('all_conections', router.all_connections)
 router.unlink(sv_to)
+print(router.all_connections)
 sv_from.send_data(Data("Hello", sv_to.get_ip()))
 router.send_data()
 msg_lst_to = sv_to.get_data()
 assert msg_lst_to == [], "метод get_data() вернул неверные данные, возможно, неправильно работает метод unlink()"
+
+#
+#
+# # a = [1,2,3,4]
+# # while a:
+# #     f = a.pop()
+#     print(f)
